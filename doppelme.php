8094c087cef750cdd36abfe65956025c533e6550
DoppelMe <DoppelMe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-26 19:27:53 +0000

    support for buddypress 1.7
    git-svn-id: https://plugins.svn.wordpress.org/doppelme-avatars/trunk@732350 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- doppelme.php
+++ doppelme.php
@@ -4,7 +4,7 @@
 Plugin Name: DoppelMe Avatars
 Plugin URI: http://www.doppelme.com
 Description: Allow your users to create their own avatars within your BuddyPress installation.
-Version: 1.00
+Version: 1.02
 Author: DoppelMe
 License: GPL2
 */
@@ -17,27 +17,4 @@ function bp_doppelme_init() {
     
 }
 add_action( 'bp_include', 'bp_doppelme_init' );
-
-
-/*
-
-// Put setup procedures to be run when the plugin is activated 
-function bp_doppelme_activate() {
-	global $wpdb;
-
-	//require_once( ABSPATH . 'wp-admin/upgrade-functions.php' );
-	//update_site_option( 'bp-example-db-version', BP_EXAMPLE_DB_VERSION );
-}
-register_activation_hook( __FILE__, 'bp_doppelme_activate' );
-
-
-
-
-// On deacativation, clean up anything your component has added. 
-function bp_doppelme_deactivate() {
-	//do  nothing
-}
-register_deactivation_hook( __FILE__, 'bp_doppelme_deactivate' );
-
-*/
 ?>
